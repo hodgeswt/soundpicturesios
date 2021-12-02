ea5b9292afbf7d405da6262a7cd0205327792db9
Will Hodges <hodgeswt@users.noreply.github.com>
2021-12-02 00:15:17 -0500

    v1.0 is working!

--- sound pictures/RecordView.swift
+++ sound pictures/RecordView.swift
@@ -15,14 +15,29 @@ struct RecordView: View {
     @State var recording = false
     @State var recorded = false
     
+    @State var receivedImages = false
+    @State var imageJson: [String: String]! = nil
+    
     var url: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("sound.m4a")
     
     @ObservedObject var recorder = Recorder()
     @ObservedObject var player = Player()
     
+    private func bytes(url: URL) -> [UInt8]? {
+        do {
+            // Get the raw data from the file.
+            let rawData: Data = try Data(contentsOf: url)
+            // Return the raw data as an array of bytes.
+            return [UInt8](rawData)
+        } catch {
+            // Couldn't read the file.
+            return nil
+        }
+    }
     
+    @ViewBuilder
     var body: some View {
-        VStack {
+        ScrollView {
             Button(action: {
                 
                 if !self.buttonState {
@@ -75,6 +90,67 @@ struct RecordView: View {
                 }
             }
             
+            TripleSpacer()
+            
+            Button(action: {
+                let fileManager = FileManager.default
+                let dir = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                let directoryContents = try! fileManager.contentsOfDirectory(at: dir, includingPropertiesForKeys: nil)
+                
+                for file in directoryContents {
+                    if file.path.hasSuffix("sound.m4a") {
+                        // If our file does exist
+                        let serverUrl = URL(string: "http://localhost:5000/sound_picture")
+                        
+                        let urlSession = URLSession.shared
+                        let boundary = UUID().uuidString
+                        var data = Data()
+                        
+                        var urlRequest = URLRequest(url: serverUrl!)
+                        urlRequest.httpMethod = "POST"
+                        
+                        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+                        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+                        data.append("Content-Disposition: form-data; name=\"data\"; filename=\"data.m4a\"\r\n".data(using: .utf8)!)
+                        data.append("Content-Type: \"content-type header\"\r\n\r\n".data(using: .utf8)!)
+                        
+                        if let bytes: [UInt8] = bytes(url: file) {
+                            for byte in bytes {
+                                data.append(byte)
+                            }
+                            
+                            data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+                            urlSession.uploadTask(with: urlRequest, from: data, completionHandler: { responseData, response, error in
+                                    if error == nil {
+                                        let jsonData = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments)
+                                                if let json = jsonData as? [String: String] {
+                                                    self.imageJson = json
+                                                    self.receivedImages = true
+                                                }
+                                    }
+                                }
+                            ).resume()
+                        }
+                    }
+                }
+            }) {
+                if recorded {
+                    Text("convert")
+                }
+            }
+            
+            if self.receivedImages {
+                Image(uiImage: UIImage(data: Data(base64Encoded: self.imageJson["orig"]!)!)!)
+                    .resizable()
+                    .scaledToFit()
+                Image(uiImage: UIImage(data: Data(base64Encoded: self.imageJson["new"]!)!)!)
+                    .resizable()
+                    .scaledToFit()
+                Image(uiImage: UIImage(data: Data(base64Encoded: self.imageJson["avg"]!)!)!)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
             Spacer()
         }
         .alert("Failed to Record", isPresented: $showRecordingFailAlert) {
@@ -82,6 +158,7 @@ struct RecordView: View {
                 
             }
         }
+        .padding()
         .navigationTitle("record")
         .navigationBarTitleDisplayMode(.inline)
     }
